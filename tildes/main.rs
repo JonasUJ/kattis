14de8bea2674a1e2cd86a7d667e9ffad4c750356
JonasUJ <jonas@ulsnes.dk>
2022-01-25 20:56:43 +0100

    Add tildes

---
+++ tildes/main.rs
@@ -0,0 +1,74 @@
+use std::io::{self, BufRead};
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Site(usize);
+
+#[derive(Debug)]
+struct UnionFind {
+    id: Vec<Site>,
+    sz: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(count: usize) -> Self {
+        UnionFind {
+            id: (0..count).map(Site).collect(),
+            sz: vec![1; count],
+        }
+    }
+
+    fn find(&self, mut site: Site) -> Site {
+        while site != self.id[site.0] {
+            site = self.id[site.0];
+        }
+
+        site
+    }
+
+    fn union(&mut self, a: Site, b: Site) {
+        let a = self.find(a);
+        let b = self.find(b);
+
+        if a == b {
+            return;
+        }
+
+        if self.sz[a.0] < self.sz[b.0] {
+            self.id[a.0] = b;
+            self.sz[b.0] += self.sz[a.0];
+        } else {
+            self.id[b.0] = a;
+            self.sz[a.0] += self.sz[b.0];
+        }
+    }
+
+    fn size(&self, site: Site) -> usize {
+        let set = self.find(site);
+        self.sz[set.0]
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut line = String::new();
+    stdin.read_line(&mut line);
+    let mut uf = UnionFind::new(line.split(' ').next().unwrap().parse().unwrap());
+    let mut output = Vec::<String>::new();
+
+    for line in stdin.lock().lines().map(Result::unwrap) {
+        let mut iter = line.split_whitespace();
+        match iter.next() {
+            Some("t") => uf.union(
+                Site(iter.next().unwrap().parse::<usize>().unwrap() - 1),
+                Site(iter.next().unwrap().parse::<usize>().unwrap() - 1),
+            ),
+            Some("s") => output.push(
+                uf.size(Site(iter.next().unwrap().parse::<usize>().unwrap() - 1))
+                    .to_string(),
+            ),
+            _ => panic!("invalid command"),
+        }
+    }
+
+    print!("{}", output.join("\n"));
+}
